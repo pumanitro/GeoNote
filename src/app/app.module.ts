e3e2dee1b2a27866c022c329e40e4e5cccaa7b31
pumanitro <pumanitro@gmail.com>
2017-04-22 00:28:23 +0200

    FCK YEAH! Added that shitty menu button xD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
