56a78e9a3002d43cfed8ce738084daae6519e693
pumanitro <pumanitro@gmail.com>
2017-05-07 00:37:41 +0200

    Splitted map logic into map-manager service.

---
+++ src/pages/map/map-manager.service.ts
@@ -0,0 +1,82 @@
+/**
+ * Created by puman on 06.05.2017.
+ */
+
+import {Injectable} from "@angular/core";
+import { Geolocation } from '@ionic-native/geolocation';
+
+declare let google;
+
+@Injectable()
+export class MapManagerService {
+  private map: any;
+  private mapElement;
+
+  constructor( public geolocation: Geolocation){
+  }
+
+  mapInit(mapElement){
+    this.mapElement = mapElement;
+  }
+
+  loadMap(){
+
+    this.geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 18,
+        zoomControl: false,
+        /*
+         zoomControlOptions: {
+         position: google.maps.ControlPosition.RIGHT_TOP
+         },*/
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+      //Adding a range circle to the center of our cords:
+      let cityCircle = new google.maps.Circle({
+        strokeColor: '#508fff',
+        strokeOpacity: 0.2,
+        strokeWeight: 2,
+        fillColor: '#508fff',
+        fillOpacity: 0.1,
+        map: this.map,
+        center: this.map.getCenter(),
+        radius: 100
+      });
+
+      //Displaying marker in center of the map
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: this.map.getCenter()
+      });
+
+      let content = "<h4>Information!</h4>";
+
+      this.addInfoWindow(marker, content);
+
+    }, (err) => {
+      console.log(err);
+    });
+
+  }
+
+  private addInfoWindow(marker, content){
+
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
+
+}
--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { Geolocation } from '@ionic-native/geolocation';
+
+import {MapManagerService} from "./map-manager.service";
 
 // import { Materialize } from 'materialize-css';
 
@@ -15,78 +16,21 @@ declare let google;
 @Component({
   selector: 'page-map',
   templateUrl: 'map.html',
+  providers: [MapManagerService]
 })
 export class Map {
 
   @ViewChild('map') mapElement: ElementRef;
-  map: any;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public mapManagerService: MapManagerService) {
 
   }
 
   ionViewDidLoad(){
-    this.loadMap();
+    this.mapManagerService.mapInit(this.mapElement);
+    this.mapManagerService.loadMap();
   }
 
-  loadMap(){
 
-    this.geolocation.getCurrentPosition().then((position) => {
-
-      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-
-      let mapOptions = {
-        center: latLng,
-        zoom: 18,
-        zoomControl: false,
-        /*
-        zoomControlOptions: {
-          position: google.maps.ControlPosition.RIGHT_TOP
-        },*/
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
-
-      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-
-      //Adding a range circle to the center of our cords:
-      let cityCircle = new google.maps.Circle({
-        strokeColor: '#508fff',
-        strokeOpacity: 0.2,
-        strokeWeight: 2,
-        fillColor: '#508fff',
-        fillOpacity: 0.1,
-        map: this.map,
-        center: this.map.getCenter(),
-        radius: 100
-      });
-
-      //Displaying marker in center of the map
-      let marker = new google.maps.Marker({
-        map: this.map,
-        animation: google.maps.Animation.DROP,
-        position: this.map.getCenter()
-      });
-
-      let content = "<h4>Information!</h4>";
-
-      this.addInfoWindow(marker, content);
-
-    }, (err) => {
-      console.log(err);
-    });
-
-  }
-
-  addInfoWindow(marker, content){
-
-    let infoWindow = new google.maps.InfoWindow({
-      content: content
-    });
-
-    google.maps.event.addListener(marker, 'click', () => {
-      infoWindow.open(this.map, marker);
-    });
-
-  }
 
 }
