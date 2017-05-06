496c0ee0f4b96685b7721697ff70949cdb93bdba
pumanitro <pumanitro@gmail.com>
2017-05-06 22:37:00 +0200

    Created add note fab button and removed map resizing buttons.

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
 
+import { Materialize } from 'materialize-css';
+
 declare let google;
 
 /**
@@ -36,10 +38,27 @@ export class Map {
       let mapOptions = {
         center: latLng,
         zoom: 15,
+        zoomControl: false,
+        /*
+        zoomControlOptions: {
+          position: google.maps.ControlPosition.RIGHT_TOP
+        },*/
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+
+      //Displaying marker in center of the map
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: this.map.getCenter()
+      });
+
+      let content = "<h4>Information!</h4>";
+
+      this.addInfoWindow(marker, content);
 
     }, (err) => {
       console.log(err);
@@ -47,6 +66,16 @@ export class Map {
 
   }
 
+  addInfoWindow(marker, content){
 
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+
+  }
 
 }
